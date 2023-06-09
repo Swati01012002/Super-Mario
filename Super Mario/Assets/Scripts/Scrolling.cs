f153a0e1d03f960772298e43fe7d38b0337183e5
Swati01012002 <swati2111010@akgec.ac.in>
2023-06-10 02:58:12 +0530

    Transformed camera position in underground

--- Super Mario/Assets/Scripts/Scrolling.cs
+++ Super Mario/Assets/Scripts/Scrolling.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Scrolling : MonoBehaviour
 {
     private  Transform player; //reference to game object position, rotation
+    public float height = 6.5f;
+    public float undergroundHeight = -9.5f;
 
     private void Awake()
     {
@@ -16,5 +18,12 @@ public class Scrolling : MonoBehaviour
         Vector3 cameraPosition = transform.position; //for current camera position
         cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x); //for moving in only right direction
         transform.position = cameraPosition; //to update camera position
+    }
+
+    public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = underground ? undergroundHeight : height;
+        transform.position = cameraPosition;
     }
 }
